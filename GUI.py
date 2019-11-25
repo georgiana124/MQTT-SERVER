c0c33cf21ccccb78737aace26eca7632cd10eac6
picolo190 <strilciuc_gabriel98@yahoo.ro>
2019-11-25 13:38:56 +0200

    The client object is created and manipulated by GUI

--- GUI.py
+++ GUI.py
@@ -1,43 +1,37 @@
 from tkinter import *
+from client import *
+
 
 class GUI(object):
 
     def __init__(self):
-        self._root = Tk()
-        self._root.geometry("1400x900")
-        self._root.title("MQTT Client")
-        self._button1 = Button(self._root, text="Close", command=self._root.quit)
-        self._button1.place(bordermode=OUTSIDE, x=1200, y=800)
-        self._root.bind("<Button-1>", self.motion)
-        self._v1 = StringVar()
-        self._v2 = StringVar()
-        self._isClicked = BooleanVar
-        self._isClicked = False
+        self.__root = Tk()
+        self.__root.geometry("1400x900")
+        self.__root.title("MQTT Client")
+        self.__button1 = Button(self.__root, text="Close", command=self.__root.quit)
+        self.__button1.place(bordermode=OUTSIDE, x=1200, y=800)
 
-        Label(self._root, text='Username').place(x=500, y=300)
-        e1 = Entry(self._root)
-        e1.focus_set();
+        def motion(event):
+            print(event.x, event.y)
+
+        self.__root.bind("<Button-1>", motion)
+
+        Label(self.__root, text='Username').place(x=500, y=300)
+        e1 = Entry(self.__root)
+        e1.focus_set()
 
         e1.place(x=590, y=300)
 
         def callback():
-            # Get the strings from the Entries
-            self.v1 = e1.get()
-            self.v2 = e2.get()
-            self._isClicked = True
+            # Create a client object when the connect button is pressed
+            self.__client = Client(e1.get(), e2.get())
 
-        Label(self._root, text='Password').place(x=500, y=330)
-        e2 = Entry(self._root)
+        Label(self.__root, text='Password').place(x=500, y=330)
+        e2 = Entry(self.__root)
         e2.place(x=590, y=330)
 
-        b = Button(self._root, text="Connect", width=10, command=callback)
+        b = Button(self.__root, text="Connect", width=10, command=callback)
         b.place(x=600, y=400)
-
-
-        self._root.update()
-
-        self._root.mainloop()
-
-    def motion(self, event):
-        print(event.x, event.y)
+        self.__root.update()
+        self.__root.mainloop()
 
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import client as client
 from GUI import *
 
 my_gui = GUI()
 
-if(my_gui._isClicked):
-    client = client.Client("abc", username=my_gui.v1, password=my_gui.v2)
-    client.connect()
+
