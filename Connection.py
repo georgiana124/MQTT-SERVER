5d4e7f3434cc1ab36b8174c8fd7c59951355f5bc
picolo190 <strilciuc_gabriel98@yahoo.ro>
2019-11-29 16:25:38 +0200

    Update Connection class fields

--- Connection.py
+++ Connection.py
@@ -5,15 +5,15 @@ import traceback
 class Connection(object):
 
     def __init__(self):
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._socket.settimeout(30)
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.__socket.settimeout(30)
         host_ip = socket.gethostbyname('mqtt.eclipse.org')
         print("ip:", host_ip)
         try:
-            self._socket.connect((host_ip, 1883))
+            self.__socket.connect((host_ip, 1883))
         except socket.error:
             traceback.print_exc()
 
     def send(self, packet):
-        self._socket.send(packet)
+        self.__socket.send(packet)
