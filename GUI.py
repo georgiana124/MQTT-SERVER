fb5da0f9ebe0e8929efb7bb72f7672e6ece6f2e8
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-01-14 19:06:44 +0200

    update

--- GUI.py
+++ GUI.py
@@ -46,8 +46,8 @@ class GUI:
         self.create_connect_gui()
 
     def __send_callback(self):
-        self.__text_box_receive.delete('1.0', END) #Delete text box content before showing new published content
-        #self.__client.publish()
+        self.__text_box_receive.delete('1.0', END)  # Delete text box content before showing new published content
+        # self.__client.publish()
         self.__text_box_receive.insert(INSERT, "ASC")
         self.__text_box_receive.config(state=DISABLED)
 
